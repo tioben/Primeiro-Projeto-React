6f6f0a7b903e2347dc4e91be667e6bb097026d70
Gustavo Martins <tioben@outlook.com.br>
2024-05-23 08:36:33 -0300

    23/05/2024

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,30 +1,32 @@
-import styles from './Navbar.module.css'
-import {Link} from 'react-router-dom'
-import Container from './Container'
-import logo from '../../assets/costs_logo.png'
+import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
+import Container from './Container';
+import logo from '../../assets/costs_logo.png';
 
-function Navbar(){
-    return(
-        <nav className={styles.navbar}>
-            <Container>
-                <Link to="/"><img src={logo} alt="Costs" /></Link>
-                <ul className={styles.list}>
-                    <li className={styles.item}>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link to="/projects">Projetos</Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link to="/contact">Contato</Link>
-                    </li>
-                    <li className={styles.item}>
-                        <Link to="/company">Empresa</Link>
-                    </li>
-                </ul>
-            </Container>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <Container customClass="total-width">
+        <Link to="/">
+          <img src={logo} alt="Costs" />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/projects">Projetos</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/contact">Contato</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to="/company">Empresa</Link>
+          </li>
+        </ul>
+      </Container>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,17 +1,40 @@
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import styles from './Projects.module.css';
 
 import Container from '../layout/Container';
 import Message from '../layout/Message';
 import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../Project/ProjectCard';
+import Loading from '../layout/Loading';
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [removeLoading, setRemoveLoading] = useState(false);
+
   const location = useLocation();
   let message = '';
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:5000/projects', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((resp) => resp.json())
+        .then((data) => {
+          setProjects(data);
+          setRemoveLoading(true);
+        })
+        .catch((err) => console.log(err));
+    }, 300);
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -21,7 +44,20 @@ function Projects() {
       </div>
       {message && <Message msg={message} type="success" />}
       <Container customClass="start">
-        <p>projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project.category.name}
+            />
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>Não há projetos cadastrados!</p>
+        )}
       </Container>
     </div>
   );
